914fff1abb467b31db61459e142d289bdbed2d78
Jesú Márquez <jesumarquez@gmail.com>
2018-01-16 15:58:56 -0300

    add and edit

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express'),
     app = express(),
     bodyParser = require('body-parser'),
     port = process.env.PORT || 8080,
-    usersStorage = {'users': [{'name':'a1', 'enabled': true}, {'name': 'a2', 'enabled': false}]}
+    usersStorage = {'users': [{id: 1, name:'a1', enabled: true}, {id: 2, name: 'a2', enabled: false}]}
 
 app.use(express.static('public'))
 app.use('/scripts', express.static(__dirname + '/node_modules/'))
@@ -14,8 +14,20 @@ app.get('/user', function (req, res) {
 })
 
 app.post('/user', function(req, res){
-    usersStorage.users.push(JSON.parse(req.body.user))
+    var user = JSON.parse(req.body.user)
+    user.id = usersStorage.users.length + 1
+    usersStorage.users.push(user)
     res.json(usersStorage)
+})
+
+app.put('/user', function (req, res) {
+    var u = JSON.parse(req.body.user)
+    var user = usersStorage.users.find(function (element) {
+        return element.id === u.id
+    })
+    user.name = u.name
+    user.enabled = u.enabled
+    res.json(usersStorage)    
 })
 
 app.listen(port, function () {
--- public/js/app.js
+++ public/js/app.js
@@ -9,23 +9,59 @@ $(function(){
             }
         }
         
+        /**
+         * Properties
+         */
         ko.mapping.fromJS(data, {
                 users: userMapping
             }, self)
         
         self.user = ko.observable(null)
+        self.mode = ko.observable('list')
+        
+        /**
+         * Methods
+         */
         self.changeValue = function(u){
-            u.enabled(!u.enabled())
+            //u.enabled(!u.enabled())
         }
         self.newUser = function(){
             self.user({name: '', enabled: false})
+            self.mode('new')
+        }
+        self.edit = function(u){
+            self.user(u)
+            self.mode('edit')
         }
         self.save = function(){
             var u = ko.toJSON(self.user)
-            $.post('/user', {user: u})
-                .then(function(res){
+            var type = self.mode() === 'new' ? 'POST' : 'PUT'
+            // $.post('/user', {user: u})
+            //     .then(function(res){
+            //         ko.mapping.fromJS(res, self)
+            //         self.user(null)
+            //         self.mode('list')
+            // })
+
+            $.ajax({
+                url: '/user',
+                type: type,
+                contentType: "application/json",
+                data: JSON.stringify({user: u}),
+                dataType: 'json',
+                success: function(res){
                     ko.mapping.fromJS(res, self)
-                })
+                    self.user(null)
+                    self.mode('list')
+                },
+                error: function(jqXHR, textStatus, errorThrown){
+                    console.error(textStatus, errorThrown)
+                }
+            })
+        }
+        self.cancel = function(){
+            self.user(null)
+            self.mode('list')
         }
     }
 
