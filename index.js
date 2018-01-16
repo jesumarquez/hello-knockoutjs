82e2a28ef2eda5ef726ec1ded11ac31daafca00e
Jesú Márquez <jesumarquez@gmail.com>
2018-01-16 14:14:27 -0300

    add new user

--- index.js
+++ index.js
@@ -1,15 +1,23 @@
 var express = require('express'),
     app = express(),
+    bodyParser = require('body-parser'),
     port = process.env.PORT || 8080,
-    users = {'users': [{'name':'a1'}, {'name': 'a2'}]};
+    usersStorage = {'users': [{'name':'a1', 'enabled': true}, {'name': 'a2', 'enabled': false}]}
 
-app.use(express.static('public'));
-app.use('/scripts', express.static(__dirname + '/node_modules/'));
+app.use(express.static('public'))
+app.use('/scripts', express.static(__dirname + '/node_modules/'))
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({ extended: true }))
 
-app.get('/users', function (req, res) {
-    res.send(users); 
+app.get('/user', function (req, res) {
+    res.json(usersStorage) 
+})
+
+app.post('/user', function(req, res){
+    usersStorage.users.push(JSON.parse(req.body.user))
+    res.json(usersStorage)
 })
 
 app.listen(port, function () {
-    console.log('Server is running in http://localhost:' +  port);
+    console.log('Server is running in http://localhost:' +  port)
 })
--- public/js/app.js
+++ public/js/app.js
@@ -1,19 +1,35 @@
 $(function(){
     function UserViewModel(data){
         var self = this
+        
         var userMapping = {
             create: function(options){
                 var vm = ko.mapping.fromJS(options.data)
                 return vm
             }
         }
+        
         ko.mapping.fromJS(data, {
-            users: userMapping
-        }, self)
+                users: userMapping
+            }, self)
+        
         self.user = ko.observable(null)
+        self.changeValue = function(u){
+            u.enabled(!u.enabled())
+        }
+        self.newUser = function(){
+            self.user({name: '', enabled: false})
+        }
+        self.save = function(){
+            var u = ko.toJSON(self.user)
+            $.post('/user', {user: u})
+                .then(function(res){
+                    ko.mapping.fromJS(res, self)
+                })
+        }
     }
 
-    $.get('/users')
+    $.get('/user')
         .then(function (res) {
             ko.applyBindings(new UserViewModel(res))
         })
